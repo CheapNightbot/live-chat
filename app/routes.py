7e9a3a3db550f50d462cbb3b0d07a8982e67cbe2
Cheap Nightbot <151442433+CheapNightbot@users.noreply.github.com>
2025-07-04 03:34:13 +0900

    Simple // test sending back response

--- app/routes.py
+++ app/routes.py
@@ -13,4 +13,12 @@ def index():
 
 @socketio.on("send_message")
 def handle_message(data):
+    user_message = data["msg"]
+    if "hello" in user_message.lower():
+        response = "Hello! (^///^)"
+    else:
+        response = "Me don't know what you mean! （o´・ェ・｀o）"
+
+    # First sending user message, then our response
     emit("receive_message", data, broadcast=True)
+    emit("receive_message", {"msg": response}, broadcast=True)
